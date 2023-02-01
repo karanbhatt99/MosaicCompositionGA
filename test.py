45277a6198af5fb34028fe2a2a1f33a5c1d6ebad
Umer Usman <87089227+omrusman@users.noreply.github.com>
2023-02-01 15:17:17 +0000

    Test Commit
    testing test.py file

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
-print("test")
+#Importing Image
+image = imread("./images/kittens.jpg",as_gray=True)
+
+#Resizing Image
+image = resize(image,(1000,1000))
+
+#Showing Image
+fig = plt.figure(figsize=(7,7))
+plt.grid(True)
+plt.imshow(image,cmap=plt.cm.gray)
+
+#Making tiles shapes, shuffling the image and putting it all together
+image_shape = (200,200)
+image_blocks_original = view_as_blocks(image,block_shape=image_shape)
+
+image_blocks = image_blocks_original
+image_blocks = image_blocks.reshape((-1,) + image_shape)
+
+np.random.shuffle(image_blocks)
+image_montage = montage2d(image_blocks)
+
+#Showing Shuffle Image
+fig = plt.figure(figsize=(7,7))
+plt.imshow(image_montage,cmap=plt.cm.gray)
